8093900b969f6d8e1f99f24c66c005297b03d073
Guilherme Bezerra <gbsantos.it@gmail.com>
2023-12-28 19:42:18 -0300

    ✨ feat: create register endpoint

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,45 @@
 import express from 'express';
+import { prisma } from './database/prisma';
 
 const app = express();
 
-app.get('/ping', (request, response) => {
+app.use(express.json());
+
+app.get('/ping',  (request, response) => {
   return response.status(200).send('Server is running.');
 });
 
+app.post('/create', async (request, response) => {
+  const {
+    cpf_cnpj,
+    producer_name,
+    farm_name,
+    city,
+    state,
+    agricultural_area_hectares,
+    vegetation_area_hectares,
+    total_area_hectares,
+    planted_crops
+  } = request.body;
+
+  const ruralProducer = await prisma.ruralProducer.create({
+    data: {
+      cpf_cnpj,
+      producer_name,
+      farm_name,
+      city,
+      state,
+      agricultural_area_hectares,
+      vegetation_area_hectares,
+      total_area_hectares,
+      planted_crops
+    }
+  });
+
+  return response.status(201).json(ruralProducer);
+
+});
+
 app.listen(3333, () => {
-  console.log('🚀 HTTP Server Running!')
+  console.log('🚀 HTTP Server Running!');
 });
