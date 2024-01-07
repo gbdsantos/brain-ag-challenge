01bf11bafe979eda049543734b31bb2087af0d3c
Guilherme Bezerra <gbsantos.it@gmail.com>
2024-01-07 19:17:45 -0300

    🧪 test: create tests to business rules and validations
    - Sum total arable area and vegetation area vs total area of the farm
    - CPF and CNPJ validations
    - Endpoint that return totals: number of farms, by state, by planted crops (cultures)

--- src/test/rural-producer-controller.test.ts
+++ src/test/rural-producer-controller.test.ts
@@ -7,7 +7,7 @@ import { app } from "../app"
 
 describe("Rural producer routes", () => {
   beforeEach(() => {
-    execSync('npx prisma migrate reset -f')
+    execSync('npx prisma migrate reset -f');
   })
 
   it("Should be able to create a new rural producer", async () => {
@@ -23,6 +23,36 @@ describe("Rural producer routes", () => {
       planted_crops: ["banana", "laranja"]
     })
     .expect(201);
+  });
+
+  it("Should not be able register rural producer if sum of arable area and vegetation be greater than the total area of the farm", async() => {
+    await request(app).post('/create').send({
+      cpf_cnpj: "93191298002",
+      producer_name: "Marcos Oliveira",
+      farm_name: "Fazenda Oliveira",
+      city: "Salvador",
+      state: "bahia",
+      agricultural_area_hectares: 50,
+      vegetation_area_hectares: 50,
+      total_area_hectares: 50,
+      planted_crops: ["banana", "laranja"]
+    })
+    .expect(400);
+  });
+
+  it("Should not be able register rural producer if CPF or CNPJ be not valid", async () => {
+    await request(app).post('/create').send({
+      cpf_cnpj: "11111111111",
+      producer_name: "Marcos Oliveira",
+      farm_name: "Fazenda Oliveira",
+      city: "Salvador",
+      state: "bahia",
+      agricultural_area_hectares: 50,
+      vegetation_area_hectares: 50,
+      total_area_hectares: 50,
+      planted_crops: ["banana", "laranja"]
+    })
+    .expect(400);
   });
 
   it("Should be able to list all rural producers", async () => {
@@ -139,5 +169,30 @@ describe("Rural producer routes", () => {
     await request(app)
     .delete(`/delete/${ruralProducerCpfCnpj}`)
     .expect(204);
+  });
+});
+
+describe("Dashboard routes", () => {
+  beforeEach(() => {
+    execSync('npx prisma migrate reset -f');
+  });
+
+  it("Should be able to list a total: number of farms, by state and planted crops", async () => {
+    const totals = await request(app)
+      .get('/totals')
+      .expect(200);
+
+    expect(totals.body).toEqual(
+      expect.objectContaining({
+        totals: {
+          farms: 0,
+          area_hectares: 0,
+          agricultural_area: 0,
+          vegetation_area: 0,
+          by_state: [],
+          by_planted_crop: []
+        }
+      })
+    )
   });
 });
